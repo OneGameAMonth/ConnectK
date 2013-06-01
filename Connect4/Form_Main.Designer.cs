20f576dd1ed94915362dbaaeaeba9f7cb169c367
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-06-01 16:47:01 -0430

    Painting it black

--- Connect4/Form_Main.Designer.cs
+++ Connect4/Form_Main.Designer.cs
@@ -28,98 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView_game = new System.Windows.Forms.DataGridView();
+            this.numericUpDown_rows = new System.Windows.Forms.NumericUpDown();
             this.button_start = new System.Windows.Forms.Button();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.numericUpDown_collumns = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_players = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox_currentPlayer = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_game)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_rows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_collumns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_players)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridView_game
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 24);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(737, 350);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView_game.AllowUserToAddRows = false;
+            this.dataGridView_game.AllowUserToDeleteRows = false;
+            this.dataGridView_game.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_game.Location = new System.Drawing.Point(13, 24);
+            this.dataGridView_game.Name = "dataGridView_game";
+            this.dataGridView_game.RowTemplate.Height = 24;
+            this.dataGridView_game.Size = new System.Drawing.Size(917, 455);
+            this.dataGridView_game.TabIndex = 0;
             // 
-            // numericUpDown1
+            // numericUpDown_rows
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(64, 389);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.numericUpDown_rows.Location = new System.Drawing.Point(143, 496);
+            this.numericUpDown_rows.Maximum = new decimal(new int[] {
             50,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.numericUpDown_rows.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(57, 22);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            1,
+            this.numericUpDown_rows.Name = "numericUpDown_rows";
+            this.numericUpDown_rows.Size = new System.Drawing.Size(57, 22);
+            this.numericUpDown_rows.TabIndex = 1;
+            this.numericUpDown_rows.Value = new decimal(new int[] {
+            10,
             0,
             0,
             0});
             // 
             // button_start
             // 
-            this.button_start.Location = new System.Drawing.Point(675, 389);
+            this.button_start.Location = new System.Drawing.Point(754, 496);
             this.button_start.Name = "button_start";
             this.button_start.Size = new System.Drawing.Size(75, 23);
             this.button_start.TabIndex = 2;
             this.button_start.Text = "Start";
             this.button_start.UseVisualStyleBackColor = true;
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(213, 390);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(57, 22);
-            this.numericUpDown2.TabIndex = 3;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 393);
+            this.label1.Location = new System.Drawing.Point(92, 500);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Rows";
+            this.label1.Text = "Rows:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 392);
+            this.label2.Location = new System.Drawing.Point(221, 499);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Collumns";
+            this.label2.Text = "Collumns:";
+            // 
+            // numericUpDown_collumns
+            // 
+            this.numericUpDown_collumns.Location = new System.Drawing.Point(292, 498);
+            this.numericUpDown_collumns.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown_collumns.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_collumns.Name = "numericUpDown_collumns";
+            this.numericUpDown_collumns.Size = new System.Drawing.Size(57, 22);
+            this.numericUpDown_collumns.TabIndex = 6;
+            this.numericUpDown_collumns.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_players
+            // 
+            this.numericUpDown_players.Location = new System.Drawing.Point(465, 497);
+            this.numericUpDown_players.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown_players.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_players.Name = "numericUpDown_players";
+            this.numericUpDown_players.Size = new System.Drawing.Size(57, 22);
+            this.numericUpDown_players.TabIndex = 8;
+            this.numericUpDown_players.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 500);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total Players:";
+            // 
+            // groupBox_currentPlayer
+            // 
+            this.groupBox_currentPlayer.Location = new System.Drawing.Point(645, 498);
+            this.groupBox_currentPlayer.Name = "groupBox_currentPlayer";
+            this.groupBox_currentPlayer.Size = new System.Drawing.Size(104, 22);
+            this.groupBox_currentPlayer.TabIndex = 0;
+            this.groupBox_currentPlayer.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 500);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Current Player";
             // 
             // Form_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(770, 431);
+            this.ClientSize = new System.Drawing.Size(942, 535);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox_currentPlayer);
+            this.Controls.Add(this.numericUpDown_players);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown_collumns);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.button_start);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown_rows);
+            this.Controls.Add(this.dataGridView_game);
             this.Name = "Form_Main";
             this.Text = "Form_Main";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_game)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_rows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_collumns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_players)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +203,15 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView_game;
+        private System.Windows.Forms.NumericUpDown numericUpDown_rows;
         private System.Windows.Forms.Button button_start;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown_collumns;
+        private System.Windows.Forms.NumericUpDown numericUpDown_players;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox_currentPlayer;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Connect4/Form_Main.cs
+++ Connect4/Form_Main.cs
@@ -16,5 +16,40 @@ namespace Connect4
         {
             InitializeComponent();
         }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            dataGridView_game.Rows.Clear();
+            dataGridView_game.Columns.Clear();
+
+            for (int i = 1; i <= numericUpDown_collumns.Value; i++)
+            {
+                DataGridViewColumn col = new DataGridViewTextBoxColumn();
+                col.Width = 40;
+                col.HeaderText = i.ToString();
+                dataGridView_game.Columns.Add(col);
+            }
+
+            for (int i = 1; i <= numericUpDown_rows.Value; i++)
+            {
+                dataGridView_game.Rows.Add();
+            }
+
+            disableAllBoard();
+        }
+
+        private void disableAllBoard() //aka paintItBlack()
+        {
+            foreach (DataGridViewRow row in dataGridView_game.Rows)
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    dataGridView_game[cell.ColumnIndex, cell.RowIndex].Style.BackColor = Color.Black;
+                }
+        }
+
+        private void enableAvailableSlots()
+        {
+
+        }
     }
 }
